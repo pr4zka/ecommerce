6e784bd4241a89a224af9edd36baa919e8eefd0c
ivan <ivanhakate1028@gmail.com>
2022-10-30 21:09:12 -0300

    pass

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,15 +5,18 @@ import { useState } from 'react'
 import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 import { useNavigate } from 'react-router-dom'
 import '../../src/index.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import usePassword from '../password/usePassword'
+import { Icon } from 'react-icons-kit'
+import { view } from 'react-icons-kit/ikons/view'
+import { view_off } from 'react-icons-kit/ikons/view_off'
 
 const Register = () => {
 
-  const [PasswordInputType, ToggleIcon] = usePassword();
 
- const navigate = useNavigate()
+  const navigate = useNavigate()
   const { registerRequest } = useShop()
+
+  const [type, setType] = useState('password')
+  const [icon, setIcon] = useState(view_off)
 
   const [user, setUser] = useState({
     firstName: "",
@@ -22,31 +25,40 @@ const Register = () => {
     password: ""
   })
 
+  const handleToggle = () => {
+    if (type === "password") {
+      setIcon(view)
+      setType('text')
+    } else {
+      setIcon(view_off)
+      setType('password')
+    }
+  }
 
   return (
     <div className="background">
       <div className='container-f'>
-      <h2 className='f-t'>REGISTER</h2>
-      <Formik initialValues={user}
-        onSubmit={async (values, actions) => {
-          await registerRequest(values)
-          navigate('/login')
-        }}
-      >
-        {({ handleSubmit, isSubmitting }) => (
-          <Form onSubmit={handleSubmit} className='form'>
-            <Field name="firstName" placeholder="First Name"  className='input'/>
-            <Field name="lastName" placeholder="Last Name"  className='input'/>
-            <Field type="email" name="email" placeholder="Example@gmail.com"  className='input'/>
-            <Field component={PasswordInputType} name="password"  placeholder="password" className='input' />
-            <span className='password-toogle-icon'>{ToggleIcon}</span>
-            <button className='btn-form' type='submit' disabled={isSubmitting}>{isSubmitting ? (
-              <AiOutlineLoading3Quarters />) : 'REGISTER'
-            }</button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+        <h2 className='f-t'>REGISTER</h2>
+        <Formik initialValues={user}
+          onSubmit={async (values, actions) => {
+            await registerRequest(values)
+            navigate('/login')
+          }}
+        >
+          {({ handleSubmit, isSubmitting }) => (
+            <Form onSubmit={handleSubmit} className='form'>
+              <Field name="firstName" placeholder="First Name" className='input' />
+              <Field name="lastName" placeholder="Last Name" className='input' />
+              <Field type="email" name="email" placeholder="Example@gmail.com" className='input' />
+              <input type={type} name="password" placeholder="Password" className='input' />
+              <span className='password-toogle-icon' onClick={handleToggle}><Icon icon={icon} /></span>
+              <button className='btn-form' type='submit' disabled={isSubmitting}>{isSubmitting ? (
+                <AiOutlineLoading3Quarters />) : 'REGISTER'
+              }</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   )
 }
